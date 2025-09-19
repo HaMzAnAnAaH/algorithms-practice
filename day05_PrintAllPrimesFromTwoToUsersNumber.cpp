05d4388f3d65f24f1a51b7d3088517c156bbf896
HamzaZzZ <148029535+HaMzAnAnAaH@users.noreply.github.com>
2025-09-19 23:27:53 +0300

    Add files via upload

---
+++ day05_PrintAllPrimesFromTwoToUsersNumber.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+enum enumCheckPrime {Prime = 1 , notPrime = 0};  //enums in c++ make some matters very easy and reliable :)
+
+enumCheckPrime checkPrimeNum(int rUserNum){       //tests the number if it is prime or not prime
+    
+    if(rUserNum == enumCheckPrime::notPrime || rUserNum == enumCheckPrime::Prime){
+    return enumCheckPrime::notPrime;}
+    
+    
+    for(int i=2; i<rUserNum; i++){
+        if(rUserNum%i == notPrime)
+        return enumCheckPrime::notPrime;
+    }
+    
+    return enumCheckPrime::Prime;
+}
+
+void PrintPrimesFromTwotoN(int N){
+    for(int i =2; i<=N; i++){
+       if(checkPrimeNum(i) == enumCheckPrime::Prime)
+       cout << i << "\n";
+    }
+}
+
+int ReadUserNum(){
+    int N;
+    cin >> N; cout << "\n";
+    return N;
+}
+
+int main(){
+    
+    PrintPrimesFromTwotoN(ReadUserNum());
+    return 0;
+}
