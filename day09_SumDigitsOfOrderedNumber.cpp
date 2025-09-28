7e8183bcf55436fe82c4ac4856d22516b0d2dbd0
HamzaZzZ <148029535+HaMzAnAnAaH@users.noreply.github.com>
2025-09-28 19:42:06 +0300

    Add files via upload
    This program takes a number from the user and then adds each digit to the one following it.

---
+++ day09_SumDigitsOfOrderedNumber.cpp
@@ -0,0 +1,34 @@
+//divide and conquer
+//This simple program reads a number from user and then print it in reverse order
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int ReadNum(string Message){
+    int Num;
+    cout << Message << endl;
+    cin >> Num; return Num;
+}
+
+int SumOfDigits(int Number){
+    int Remainder = 0; int sum =0;
+    while(Number > 0){
+        Remainder = Number%10; 
+        Number = Number/10;
+        sum += Remainder;
+    }
+    return sum;
+}
+
+int main(){
+    
+    
+    // Prompt the user to enter a positive number and then calculate the sum of its digits.
+    // The result is then printed to the console with an explanatory message.
+    cout << "Sum Of Digits = " 
+         << SumOfDigits(ReadNum("Please enter a positive number?"))
+         << "\n";
+    
+    return 0;
+}
