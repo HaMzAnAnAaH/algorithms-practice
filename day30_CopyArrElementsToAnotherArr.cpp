29a4d712b08a069302cc58437ae652253c0d7162
HamzaZzZ <148029535+HaMzAnAnAaH@users.noreply.github.com>
2026-01-14 22:24:13 +0300

    Add files via upload

---
+++ day30_CopyArrElementsToAnotherArr.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>    // Include cstdlib for rand() and srand() functions.
+#include <ctime>      // Include ctime for the time() function, used for seeding the random number generator.
+
+using namespace std;
+
+
+int readUserNum(string message){
+    
+    int Num;
+    do{
+        cout << message;
+        cin >> Num;
+    }while(Num < 0);
+    cout << "\n";
+    return Num;
+}
+
+
+int RandomNumber(int From, int To)
+{
+    // Calculate a random number in the range [From, To]:
+    // rand() % (To - From + 1) gives a value between 0 and (To - From),
+    // then adding From shifts this range to [From, To].
+    int randNum = rand() % (To - From + 1) + From;
+    return randNum;  // Return the generated random number.
+}
+
+void FillArrayWithRandomNumsAndCopyItIntoAnotherArray(int ArraySize[100], int &arrLength, int cpArr[100]){
+    arrLength = readUserNum("Please enter the length of array: ");
+    cout << "Array 1 elements: ";
+    for(int i=0 ; i<arrLength ; i++){
+        ArraySize[i] = RandomNumber(1, 100);
+        cpArr[i] = ArraySize[i];
+        cout << ArraySize[i] << " ";
+    }
+}
+
+void PrintSecondArrayElements(int cpArr[100], int arrLength){
+    cout << "\n\nArray 2 elements: ";
+    for(int i=0; i<arrLength; i++){
+            cout << cpArr[i] << " ";
+    }
+}
+
+int main()
+{
+    // Seed the random number generator with the current time.
+    // This ensures that the sequence of random numbers is different on each run.
+    srand((unsigned)time(NULL));
+    
+    int arr[100];
+    int cpArr[100];
+    int arrLength;
+    FillArrayWithRandomNumsAndCopyItIntoAnotherArray(arr, arrLength, cpArr);
+    PrintSecondArrayElements(cpArr, arrLength);
+    
+    return 0;
+}
